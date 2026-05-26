778e112bd01f5cc17d892501e7f2f602a602097c
Martin Pastor <martin.pastor@estudiant.enti.cat>
2026-05-26 13:52:17 +0200

    base de project
    definicions aplicades a defines.h, llibrerias aplicades a includes.h y main començat

--- escacs/escacs/defines.h
+++ escacs/escacs/defines.h
@@ -1,1 +1,17 @@
 #pragma once
+
+//Numeros necessaris per evitar magic numbers i fer bones praxis
+#define BUFFER_INPUT 1000
+#define ZERO 0
+#define UN 1
+#define DOS 2
+#define TRES 3
+#define CUATRE 4
+#define CINC 5
+#define SIS 6
+#define SET 7
+#define VUIT 8
+#define NOU 9
+#define DEU 10
+
+
