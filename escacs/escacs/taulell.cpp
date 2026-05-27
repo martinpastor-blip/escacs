e6ed4bee7b6c4ba9961449b62595a59b69722929
Pol Mates Clar <pol.mates@estudiant.enti.cat>
2026-05-27 13:03:04 +0200

    taulell.cpp contingut
    casi completat taulell cpp

--- escacs/escacs/taulell.cpp
+++ escacs/escacs/taulell.cpp
@@ -18,3 +18,59 @@ void posarFitxa(int fila, int columna, ColorFitxa color, TipusFitxa tipus)
         return;
     }
 
+    if (color == ColorFitxa::BLANC)
+    {
+        if (tipus == TipusFitxa::PEO)
+        {
+            taulell[fila][columna].simbol = " P ";
+        }
+        if (tipus == TipusFitxa::TORRE)
+        {
+            taulell[fila][columna].simbol = " T ";
+        }
+        if (tipus == TipusFitxa::CAVALL)
+        {
+            taulell[fila][columna].simbol = " H ";
+        }
+        if (tipus == TipusFitxa::ALFIL)
+        {
+            taulell[fila][columna].simbol = " B ";
+        }
+        if (tipus == TipusFitxa::REINA)
+        {
+            taulell[fila][columna].simbol = " Q ";
+        }
+        if (tipus == TipusFitxa::REI)
+        {
+            taulell[fila][columna].simbol = " K ";
+        }
+    }
+
+    if (color == ColorFitxa::NEGRE)
+    {
+        if (tipus == TipusFitxa::PEO)
+        {
+            taulell[fila][columna].simbol = " p ";
+        }
+        if (tipus == TipusFitxa::TORRE)
+        {
+            taulell[fila][columna].simbol = " t ";
+        }
+        if (tipus == TipusFitxa::CAVALL)
+        {
+            taulell[fila][columna].simbol = " h ";
+        }
+        if (tipus == TipusFitxa::ALFIL)
+        {
+            taulell[fila][columna].simbol = " b ";
+        }
+        if (tipus == TipusFitxa::REINA)
+        {
+            taulell[fila][columna].simbol = " q ";
+        }
+        if (tipus == TipusFitxa::REI)
+        {
+            taulell[fila][columna].simbol = " k ";
+        }
+    }
+}
