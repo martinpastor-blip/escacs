2214b63ad58a764f9ec8e5689bb5bb7dad42e87a
Martin Pastor <martin.pastor@estudiant.enti.cat>
2026-05-29 20:12:15 +0200

    canvis taulell.cpp , joc.h
    definicio funcions que s'utilitzaran a joc.cpp a joc.h, s'ha afegit contingut a taulell.cpp per al seu correcte funcionamient

--- escacs/escacs/joc.h
+++ escacs/escacs/joc.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "taulell.h"
+#include "defines.h"
+
+#define RANG_MINIM           UN
+#define RANG_MAXIM           VUIT
+#define DIRECCIO_BLANC      -UN
+#define DIRECCIO_NEGRE       UN
+
+#define UN_PAS               UN
+#define DOS_PASSOS           DOS
+#define SENSE_REI           -UN
+#define DIRECCIO_POSITIVA    UN
+#define DIRECCIO_NEGATIVA   -UN
+#define SENSE_DIRECCIO       ZERO
+
+#define SALT_CAVALL_LLARG    DOS
+#define SALT_CAVALL_CURT     UN
+
+// Funcions de control del joc
+void canviarTorn();
+bool estaEnEscac(ColorFitxa color);
+bool teMovimentsValids(ColorFitxa color);
+bool moureFitxa(int filaOrigen, int columnaOrigen, int filaDesti, int columnaDesti);
+
+
+// Funcions de logica de les peces
+int valorAbsolut(int numero);
+bool camiLliure(int filaOrigen, int columnaOrigen, int filaDesti, int columnaDesti);
+bool potAtacar(int filaOrigen, int columnaOrigen, int filaDesti, int columnaDesti);
+bool esMovimentValid(int filaOrigen, int columnaOrigen, int filaDesti, int columnaDesti);
--- escacs/escacs/taulell.cpp
+++ escacs/escacs/taulell.cpp
@@ -6,7 +6,6 @@ Fitxa taulell[TAMANYTAULELL][TAMANYTAULELL];
 Partida partida;
 
 
-// Assigna una fitxa a una casella del taulell i actualitza el seu simbol 
 void posarFitxa(int fila, int columna, ColorFitxa color, TipusFitxa tipus)
 {
     taulell[fila][columna].color = color;
@@ -72,5 +71,75 @@ void posarFitxa(int fila, int columna, ColorFitxa color, TipusFitxa tipus)
         {
             taulell[fila][columna].simbol = " k ";
         }
+    }
+}
+
+
+
+
+void inicialitzarTaulell()
+{
+    partida.tornActual = ColorFitxa::BLANC;
+    partida.partidaAcabada = false;
+
+    for (int fila = ZERO; fila < TAMANYTAULELL; fila++)
+    {
+        for (int columna = ZERO; columna < TAMANYTAULELL; columna++)
+        {
+            posarFitxa(fila, columna, ColorFitxa::CAP, TipusFitxa::BUIT);
+        }
+    }
+
+    for (int columna = ZERO; columna < TAMANYTAULELL; columna++)
+    {
+        posarFitxa(FILA_PEONS_NEGRE, columna, ColorFitxa::NEGRE, TipusFitxa::PEO);
+        posarFitxa(FILA_PEONS_BLANC, columna, ColorFitxa::BLANC, TipusFitxa::PEO);
+    }
+
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_TORRE_ESQ, ColorFitxa::NEGRE, TipusFitxa::TORRE);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_CAVALL_ESQ, ColorFitxa::NEGRE, TipusFitxa::CAVALL);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_ALFIL_ESQ, ColorFitxa::NEGRE, TipusFitxa::ALFIL);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_REINA, ColorFitxa::NEGRE, TipusFitxa::REINA);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_REI, ColorFitxa::NEGRE, TipusFitxa::REI);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_ALFIL_DRE, ColorFitxa::NEGRE, TipusFitxa::ALFIL);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_CAVALL_DRE, ColorFitxa::NEGRE, TipusFitxa::CAVALL);
+    posarFitxa(FILA_PECES_NEGRE, COLUMNA_TORRE_DRE, ColorFitxa::NEGRE, TipusFitxa::TORRE);
+
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_TORRE_ESQ, ColorFitxa::BLANC, TipusFitxa::TORRE);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_CAVALL_ESQ, ColorFitxa::BLANC, TipusFitxa::CAVALL);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_ALFIL_ESQ, ColorFitxa::BLANC, TipusFitxa::ALFIL);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_REINA, ColorFitxa::BLANC, TipusFitxa::REINA);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_REI, ColorFitxa::BLANC, TipusFitxa::REI);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_ALFIL_DRE, ColorFitxa::BLANC, TipusFitxa::ALFIL);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_CAVALL_DRE, ColorFitxa::BLANC, TipusFitxa::CAVALL);
+    posarFitxa(FILA_PECES_BLANC, COLUMNA_TORRE_DRE, ColorFitxa::BLANC, TipusFitxa::TORRE);
+}
+
+
+void imprimirTaulell()
+{
+    std::cout << "\nESCACS: POL MATES I MARTIN PASTOR\n";
+    std::cout << NUMEROSTAULELL;
+    std::cout << "    ------------------------\n";
+
+    for (int fila = ZERO; fila < TAMANYTAULELL; fila++)
+    {
+        std::cout << " " << TAMANYTAULELL - fila << " |";
+        for (int columna = ZERO; columna < TAMANYTAULELL; columna++)
+        {
+            std::cout << taulell[fila][columna].simbol;
+        }
+        std::cout << "|\n";
+    }
+
+    std::cout << "    ------------------------\n";
+
+    if (partida.tornActual == ColorFitxa::BLANC)
+    {
+        std::cout << "    Torn de Blanques (Majuscules)\n";
+    }
+    if (partida.tornActual == ColorFitxa::NEGRE)
+    {
+        std::cout << "    Torn de Negres (Minuscules)\n";
     }
 }
